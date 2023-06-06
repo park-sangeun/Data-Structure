3ba0456ff51cb6300d783e8cc861512ddf4c8b00
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-06-06 17:24:24 +0900

    Add files via upload

---
+++ HW1/List+Stack/Inorder.cpp
@@ -0,0 +1,84 @@
+#include "Inorder.h"
+#include <cassert>
+
+List::List() {
+	head = 0;
+	tail = 0;
+	current = 0;
+}
+List::~List() { //소멸자 생성
+	while (head != 0) //head를 버리고, head를 다음 칸으로 옮김-->앞에서부터 하나씩 제거
+	{
+		Link delNode = head;
+		head = head->next;
+		delete delNode;
+	}
+}
+void List::insert(const ListElementType& elem)
+{
+	/*Link addedNode = new Node;
+	assert(addedNode);
+	addedNode->elem = elem;
+	if (head == 0) {
+		head = addedNode;
+	}
+	else {
+		tail->next = addedNode;
+	}
+		tail = addedNode;
+		addedNode->next = 0;*/
+	//Link addedNode = new Node; //강의자료 p.28 내용
+	//assert(addedNode);
+	//addedNode->elem = elem;
+	//if (head == 0) {
+	//	addedNode->next = 0;
+	//	tail = addedNode;
+	//}
+	//else
+	//	addedNode->next = head;
+	//head = addedNode; //공통으로 발생, 뒤로 빼준 것임
+	Link addedNode(new Node);
+	assert(addedNode);
+	addedNode->elem = elem;
+	if (head == 0){
+		addedNode->next = 0;
+		head = addedNode;
+		tail = addedNode;
+	}
+	else if (elem <= head->elem){
+		addedNode->next = head;
+		head = addedNode;
+	}
+	else {
+		Link pred(head);
+		while (pred->next != 0 && pred->next->elem <= addedNode->elem)
+			pred = pred->next;
+
+		if (pred->next == 0)
+			tail = addedNode;
+		addedNode->next = pred->next;
+		pred->next = addedNode;
+	}
+}
+
+bool List::first(ListElementType& elem)
+{
+	if (head == 0)
+		return false;
+	else {
+		elem = head->elem;
+		current = head;
+		return true;
+	}
+}
+bool List::next(ListElementType& elem)
+{
+	assert(current);
+	if (current->next == 0)
+		return false;
+	else {
+		current = current->next;
+		elem = current->elem;
+		return true;
+	}
+}
---
+++ HW1/List+Stack/list.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    List l; 
+    ListElementType i; // header file defines this to be int
+    cout << "Enter items to add to list, add 0 to stop: ";
+    cin >> i;
+
+    while (i != 0) { 
+        l.insert(i); 
+        cin >> i;
+    }
+    cout << "Here are the items in the list.\n";
+    ListElementType elem;
+    bool notEmpty(l.first(elem)); 
+    while (notEmpty) { 
+        cout << elem << endl; 
+        notEmpty = l.next(elem); 
+    }
+    return 0;
+}
+
