df056e55ef34468839114f1cfa6a3753912b2809
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-06-06 17:25:41 +0900

    Add files via upload

---
+++ HW2/Queue&Hash/Queue_LinkedList.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Queue.h"
+#include <cassert>
+using namespace std;
+
+int main()
+{
+    cout << "20211522 박상은" << endl;
+    cout << "input char (if you want to end, enter .): ";
+    char c;
+    Queue <char> q;
+    while (1) {
+        cin >> c;
+        if (c == '.')
+            break;
+        q.enqueue(c);
+    }
+    while (!q.isEmpty()) {
+        cout << "Q: " << q.dequeue() << '\t';
+    }
+    return 0;
+}
